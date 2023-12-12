6566e4aa999a59be301e346a0661d6d48cabdafc
vinunarwal <vinunarwal3@gmail.com>
2023-12-12 17:00:09 -0500

    vini

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-//import './sections/css/web.css';
 import Cart from './sections/portion/Cart';
 import Account from './sections/portion/Account';
 import About from './sections/portion/About';
 
+
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <Cart />
-                {/*<Account/>*/}
-                {/*<About/>*/}
-
-            </header>
-        </div>
+        <>
+        <About/>
+        <Cart/>
+        <Account/>
+        </>
     );
 }
 
---
+++ src/sections/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import About from './portion/About'
+import Account from './portion/Account'
+import Cart from './portion/Cart'
+
+function Home() {
+  return (
+    <div>
+        <About/>
+        {/*<Account/>*/}
+        {/*<Cart/>*/}
+      
+    </div>
+  )
+}
+
+export default Home
