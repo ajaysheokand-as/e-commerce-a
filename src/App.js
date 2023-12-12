d9f03d405f15a5016309cc8a568e834ce557c416
vinunarwal <vinunarwal3@gmail.com>
2023-12-12 17:04:58 -0500

    vini

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+//import './sections/css/web.css';
 import Cart from './sections/portion/Cart';
 import Account from './sections/portion/Account';
 import About from './sections/portion/About';
 
-
 function App() {
     return (
-        <>
-        <About/>
-        <Cart/>
-        <Account/>
-        </>
+        <div className="App">
+            <header className="App-header">
+                <Cart />
+                {/*<Account/>*/}
+                {/*<About/>*/}
+
+            </header>
+        </div>
     );
 }
 
