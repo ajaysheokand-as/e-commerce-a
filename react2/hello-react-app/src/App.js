1daed0c0d85178c7e8b113d341e8e722eb3018d6
SahilSheorn <sahilks0317@gmail.com>
2024-01-08 10:48:16 +0530

    completes

---
+++ react2/hello-react-app/src/App.js
@@ -0,0 +1,18 @@
+import Navbar from'./components/Navbar';
+import Wishlist from './components/wishlist';
+// import Contact from './components/contact';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar/>
+        <Wishlist/>
+        {/* <Contact/> */}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react2/hello-react-app/src/components/Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Checkout() {
+  return (
+    <div className='checkout'>
+        <div className='heading_menu'>
+            <div className='account_menu'>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Checkout
